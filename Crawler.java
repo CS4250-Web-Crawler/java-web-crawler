97126c835bdfdb6d3b20e2a96a1c3cb37eef87c7
Mary Setian <89284003+marysetian@users.noreply.github.com>
2021-10-07 00:01:15 -0700

    Update Crawler.java

--- Crawler.java
+++ Crawler.java
@@ -1,7 +1,6 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.net.MalformedURLException;
 import java.util.HashMap;
 import org.jsoup.Jsoup;
@@ -16,10 +15,12 @@ import static com.github.pemistahl.lingua.api.Language.*;
 public class Crawler {
     public static final String EN = "ENGLISH";
     public static final String ES = "SPANISH";
-    public static final String ZH = "CHINESE";
     public static final String FR = "FRENCH";
-    public static final int MAX_CRAWL_COUNT = 50;
-    public static final String PRIMARY_SEED = "https://www.wikipedia.org/";
+    public static final int MAX_CRAWL_COUNT = 500;
+    public static final String EN_PRIMARY_SEED = "https://www.cpp.edu/";
+    public static final String FR_PRIMARY_SEED = "https://www.lepoint.fr/24h-info/monde/";
+    public static final String ES_PRIMARY_SEED = "https://www.univision.com";
+
 
     final LanguageDetector detector = LanguageDetectorBuilder.fromLanguages(ENGLISH, FRENCH, CHINESE, SPANISH).build();
 
@@ -48,39 +49,38 @@ public class Crawler {
                 //String that holds all the html content of the url
                 String htmlString = document.html();
 
-                // get first 100 characters from page and use it to detect language 
+                // get first 100 characters from page and use it to detect language
                 String bodyStr = document.select("html").text();
                 String partialStr = bodyStr.substring(0, Math.min(bodyStr.length(), 100));
                 Language detectedLanguage = detector.detectLanguageOf(partialStr);
 
                 System.out.println("The language is " + detectedLanguage.toString());
 
-                    // validate html lang
-                    if (detectedLanguage.toString().equals(lang)) {
-                        // html might no have a head containing the lang attribute, need to change the validation method
-                        System.out.println("Language Validated for URL:" + url);
+                // validate html lang
+                if (detectedLanguage.toString().equals(lang)) {
+                    // html might no have a head containing the lang attribute, need to change the validation method
+                    System.out.println("Language Validated for URL:" + url);
 
-                        // if html content in desired lang, call remove method to remove images, CSS, and JavaScript, and then add to array
-                        htmlContents[visitedLinksCount++] = remove(htmlString.toString());
-                        // get outlinks
-                        Elements linksOnPage = document.select("a[href]");
-                        // mark current url as visited and initialize the num of outlinks with 0
-                        linkCollection.put(url, 0);
-
-                        int outlinksCount = 0;
-                        // for each outlink, increment count and crawl it
-                        for (Element page : linksOnPage) {
-                            outlinksCount++;
-                            String plainUrl = page.attr("abs:href");
-                            // might not do anything if the first step of validation isn't passed
-                            crawl(plainUrl, lang);
-                        }
-
-                        // update the url with the correct outlinks count
-                        linkCollection.put(url, outlinksCount);
-                    } else {
-                        System.out.println(url + "is not " + lang + ", it is " + detectedLanguage.toString());
+                    // if html content in desired lang, call remove method to remove images, CSS, and JavaScript, and add to array
+                    htmlContents[visitedLinksCount++] = remove(htmlString.toString());
+                    // get outlinks
+                    Elements linksOnPage = document.select("a[href]");
+                    // mark current url as visited and initialize the num of outlinks with 0
+                    linkCollection.put(url, 0);
+                    int outlinksCount = 0;
+                    // for each outlink, increment count and crawl it
+                    for (Element page : linksOnPage) {
+                        outlinksCount++;
+                        String plainUrl = page.attr("abs:href");
+                        // might not do anything if the first step of validation isn't passed
+                        crawl(plainUrl, lang);
                     }
+
+                    // update the url with the correct outlinks count
+                    linkCollection.put(url, outlinksCount);
+                } else {
+                    System.out.println(url + "is not " + lang + ", it is " + detectedLanguage.toString());
+                }
             } catch(MalformedURLException e){
                 System.out.println("Error for " + url + ":" + e.getMessage());
             } catch (IOException e) {
@@ -93,12 +93,12 @@ public class Crawler {
 
     // method that removes unwanted images, CSS, and JavaScript
     public String remove(String html) {
-        Safelist wl = Safelist.relaxed();
+        Safelist sl = Safelist.relaxed();
         //remove style, script, and img tags
-        wl.removeTags("style", "script", "img");
+        sl.removeTags("style", "script", "img");
 
-        //store the cleaned HTML in a new string
-        return Jsoup.clean(html, wl);
+        //return the cleaned HTML
+        return Jsoup.clean(html, sl);
     }
 
     // iterate and write all html content to a text file in repository folder
@@ -112,7 +112,7 @@ public class Crawler {
 
             // write all content collected from content array
             for (String s : content) {
-                writer.write(s);                
+                writer.write(s);
             }
             writer.close();
 
@@ -121,19 +121,34 @@ public class Crawler {
         }
     }
 
-    public static void main(String[] args) throws IOException{
-        Crawler englishCrawler = new Crawler();
-        englishCrawler.crawl(PRIMARY_SEED, EN);
-        download(htmlContents, EN);
-
+    public static void writeToCSV(HashMap<String, Integer> linkCollection, String lang) throws IOException{
         // create csv file
         File csvFile = new File("report.csv");
-        PrintWriter output = new PrintWriter(csvFile);
+        FileWriter output = new FileWriter(csvFile, true);
 
+        output.write( "\nLanguage , " + lang + "\n");
         //iterate through hashmap elements (visited url and number of outlinks) and write to csv file
         for (HashMap.Entry<String, Integer> entry: linkCollection.entrySet()) {
             output.write(entry.getKey() + " , " + entry.getValue() + "\n");
         }
         output.close();
     }
+
+    public static void main(String[] args) throws IOException{
+        Crawler englishCrawler = new Crawler();
+        englishCrawler.crawl(EN_PRIMARY_SEED, EN);
+        download(htmlContents, EN);
+        writeToCSV(linkCollection, EN);
+
+        Crawler frenchCrawler = new Crawler();
+        frenchCrawler.crawl(FR_PRIMARY_SEED, FR);
+        download(htmlContents, FR);
+        writeToCSV(linkCollection, FR);
+
+        Crawler spanishCrawler = new Crawler();
+        spanishCrawler.crawl(ES_PRIMARY_SEED, ES);
+        download(htmlContents, ES);
+        writeToCSV(linkCollection, ES);
+    }
 }
+
